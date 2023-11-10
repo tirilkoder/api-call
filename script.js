8a39266a29a743e34ae36b3c24b077c4ad9aebbc
tirilkoder <tiril.koder@gmail.com>
2023-11-10 11:25:19 +0100

    Changed API and added paragraph

--- script.js
+++ script.js
@@ -2,19 +2,29 @@
 const boredCard = document.querySelector(".bored_card");
 
 // Fetches the data from the url and displays it in the browser
-function getActivity() {
-    fetch("https://www.boredapi.com/api/activity")
+function randomJoke() {
+    fetch("https://official-joke-api.appspot.com/random_joke")
     .then((response) => response.json())
     .then((data) => {
-    // Removes the activity paragraph before it adds a new one
+    // Removes the quote paragraph before it adds a new one
       boredCard.innerHTML = "";
       console.log(data);
-    // Create a new paragraph element with the activity text from the data object
-    const newParagraph = document.createElement("p");
-    newParagraph.innerText = data.activity;
+      
+    // Create a new paragraph element with the setup text from the data object
+    const setupParagraph = document.createElement("p");
+    setupParagraph.textContent = data.setup || "No joke found";
+    setupParagraph.className = "setup";
+    
+    // Create a new paragraph element with the punchline text from the data object
+    const punchlineParagraph = document.createElement("p");
+    punchlineParagraph.textContent = data.punchline;
+    punchlineParagraph.className = "punchline";
+   
     // Adds the new paragraph to div "bored_card"
-    boredCard.appendChild(newParagraph);
+    boredCard.appendChild(setupParagraph);
+    boredCard.appendChild(punchlineParagraph); 
     })
+    
     // If there is an error, log it to the console
     .catch((error) => console.log(error));
 }
